138ea0cd8a8e3d7e814c03fbb4f125673260b2eb
Brian Munyua <munyuabrian712@gmail.com>
2026-05-12 20:31:42 +0300

    feat: initialize Firebase SDK and update dependency to version 12.13.0

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6cIRfgsEfNxFFv0-l5cUmlSyZNVNrCow",
+  authDomain: "laundry-app-63eaa.firebaseapp.com",
+  projectId: "laundry-app-63eaa",
+  storageBucket: "laundry-app-63eaa.firebasestorage.app",
+  messagingSenderId: "583978517084",
+  appId: "1:583978517084:web:968752a7e611a4fd349c99"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
